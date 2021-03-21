dcebfe56fe16b9f19d5eac1979dd93fb57752188
Sam Suddath <srsuddath@gmail.com>
2021-03-20 20:29:50 -0400

    incremental update

--- data/SPELLS.js
+++ data/SPELLS.js
@@ -110,7 +110,7 @@ export default [
   {
     name: "Animal Messenger",
     level: "2nd",
-    castingTime: "Action",
+    castingTime: "1 Action",
     concentration: false,
     duration: "24 hours",
     school: "Enchantment",
@@ -124,7 +124,7 @@ export default [
   {
     name: "Animal Shapes",
     level: "8th",
-    castingTime: "Action",
+    castingTime: "2 Actions",
     concentration: true,
     duration: "Up to 24 hours",
     school: "Transmutation",
@@ -133,18 +133,6 @@ export default [
     text:
       "Your magic turns others into beasts. Choose any number of willing creatures that you can see within range. You transform each target into the form of a Large or smaller beast level 4 or lower. On subsequent turns, you can use 3 actions to transform affected creatures into new forms. The transformation lasts for the duration for each target, or until the target drops to 0 hit points or dies. You can choose a different form for each target. A target's game statistics are replaced by the statistics of the chosen beast, though the target retains its Intelligence, Wisdom, and Charisma scores. The target assumes the hit points of its new form, and when it reverts to its normal form, it returns to the number of hit points it had before it transformed. If it reverts as a result of dropping to 0 hit points, any excess damage carries over to its normal form. As long as the excess damage doesn't reduce the creature's normal form to 0 hit points, it isn't knocked unconscious. The creature is limited in the actions it can perform by the nature of its new form, and it can't speak or cast spells. The target's gear melds into the new form. The target can't activate, wield, or otherwise benefit from any of its equipment.",
     atHigherLevels: "",
-  },
-  {
-    name: "Summon Undead",
-    level: "3rd",
-    castingTime: "1 Minute",
-    concentration: true,
-    duration: "Up to 1 hour",
-    school: "Necromancy",
-    range: "30 feet",
-    components: "V, S",
-    text: "need to add from TCE",
-    atHigherLevels: "need to add from TCE",
   },
   {
     name: "Antilife Shell",
